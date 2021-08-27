14bbfd5f87915ede0ddc87ccdb45996675c94980
AcktarDevs <itsmeacktaryt@gmail.com>
2021-08-27 17:40:40 +0400

    😪

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const ipgeoblock = require("ipgeoblock-fixed");
 app.use(ipgeoblock({
 	geolite2: "./GeoLite2-Country.mmdb",
 	blockedCountries: [ "MU"]
+}, function (req, res) {
+	res.statusCode = 500;
+	res.end("Your country is blocked. Please use a vpn to access the website");
 }));
 
 app.get('/', function (req, res) {
